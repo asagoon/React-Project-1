06298764ee47f75deb2fa3755bc52f7bccdb0fd8
Yuta Asai <asagon.ngzk46@gmail.com>
2023-08-05 15:06:16 -0700

    first commit

---
+++ pokemon-app/src/App.js
@@ -0,0 +1,46 @@
+import { useEffect , useState } from 'react';
+import './App.css';
+import {getAllPokemon, getPokemon} from './utils/pokemon';
+
+function App() {
+  const initialURL = 'https://pokeapi.co/api/v2/pokemon';
+
+  const [loading, setLoading] = useState(true);
+  const [pokemonData, setpokemonData] = useState();
+
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      //全てのポケモンデータを取得
+      let res = await getAllPokemon(initialURL);
+      //各ポケモンの詳細なデータを取得
+      loadPokemon(res.results);
+      // console.log(res.results);
+      setLoading(false);
+    };
+   fetchPokemonData();
+  }, []);
+
+  const loadPokemon = async (data) => {
+    let _pokemonData = await Promise.all(
+    data.map((pokemon) => {
+      // console.log(pokemon);
+      let pokemonRecord = getPokemon(pokemon.url);
+      return pokemonRecord;
+    })
+    );
+    setpokemonData(_pokemonData);
+  }
+
+  console.log(pokemonData);
+
+  return (
+    <div className="App">
+      {loading ? (
+        <h1>ロード中</h1>
+      ) : <h1>ポケモンデータを取得しました</h1>}
+    </div>
+  );
+}
+
+export default App;
